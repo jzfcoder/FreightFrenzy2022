34633060ad8ea85e94028ad695a92a9b48db53c6
zflint <jzflint@gmail.com>
2022-02-21 18:59:15 -0800

    roadrunner constants for test chassis (finished)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -53,9 +53,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.033;
+    public static double kV = 0.011;
     public static double kA = 0.002;
-    public static double kStatic = 0.015;
+    public static double kStatic = 0.09894;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -64,8 +64,8 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 10;
-    public static double MAX_ACCEL = 10;
+    public static double MAX_VEL = 20;
+    public static double MAX_ACCEL = 20;
     public static double MAX_ANG_VEL = Math.toRadians(60);
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
